7a1fadc475033c862209ed1556ce08af749db1e7
Veronika Korol <veronika.korol@rds.de>
2023-01-06 12:56:43 +0100

    Projektplanung bearbeitet

--- projektplanung/script.js
+++ projektplanung/script.js
@@ -14,3 +14,42 @@ function getNextMonth() {
   document.getElementById("current-month-and-year").innerHTML = "Februar 2023";
 }
 
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, direction, switchCount = 0;
+  table = document.getElementById("sortTable");
+  switching = true;
+  direction = "asc";
+
+  while(switching) {
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i+1].getElementsByTagName("TD")[n];
+
+      if (direction == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (direction == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+      switching = true;
+      switchCount++;
+    } else {
+      if (switchCount == 0 && direction == "asc") {
+        direction = "desc";
+        switching = true;
+      }
+    }
+  }
+}
